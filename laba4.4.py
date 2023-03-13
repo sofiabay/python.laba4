5b13bc150721901cc9fe0e96f30bc16aa11875b8
sophie <sofiabayol@mail.ru>
2023-03-13 23:30:06 +0300

    add file

---
+++ laba4.4.py
@@ -0,0 +1,13 @@
+def lucky(number):
+    half = len(number) // 2
+    first_half = number[:half]
+    second_half = number[half:]
+    return sum(map(int, first_half)) == sum(map(int, second_half))
+number = input("Введите номер билета: ")
+if len(number) % 2 == 0:
+    if lucky(number):
+        print("Поздравляем! У вас счастливый билет!")
+    else:
+        print("К сожалению у вас несчатливый билет =(")
+else:
+    print("Номер должен быть четной длины")
